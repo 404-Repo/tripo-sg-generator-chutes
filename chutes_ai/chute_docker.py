d92d6e56d2183525b279d9e88e7cb016c8dbfbb9
teshate <tereshin.tesha@gmail.com>
2025-11-07 13:57:35 +0000

    updated docker image - fully buildable now

--- chutes_ai/chute_docker.py
+++ chutes_ai/chute_docker.py
@@ -1,23 +1,26 @@
 from chutes.image import Image
 
 
-chute_docker_image = (
-    Image(username="", name="tripo-sg-mesh-generator", tag="0.0")
-    .from_base("nvidia/cuda:12.8.0-devel-ubuntu24.04")
+class ChuteDockerImage:
+    def __init__(self, username: str, docker_image_name: str, tag: str):
+        self.chute_docker_image = (
+            Image(username=username, name=docker_image_name, tag=tag)
+            .from_base("nvidia/cuda:12.8.0-devel-ubuntu24.04")
 
-    # installing system libraries
-    .run_command("apt update && apt install -y python3-full python3-dev python3-pip python3-wheel git build-essential libpq-dev")
+            # installing system libraries
+            .run_command("apt update && apt install -y python3-full python3-dev python3-pip python3-wheel git build-essential libpq-dev")
 
-    # installing python packages
-    .run_command("ln -sf /usr/bin/pip3 /usr/local/bin/pip")
-    .run_command("pip install torch==2.7.1 torchvision --index-url https://download.pytorch.org/whl/cu128 --break-system-packages")
-    .run_command("CPATH='/usr/local/cuda/include:$CPATH' pip3 install diso==0.1.4 --break-system-packages")
-    .run_command("pip install git+https://github.com/404-Repo/tripo-sg-generator-chutes.git --break-system-packages")
-    .run_command("pip install --upgrade --break-system-packages chutes")
+            # installing python packages
+            .run_command("rm /usr/lib/python3*/EXTERNALLY-MANAGED")
 
-    # Set up model cache directory
-    .run_command("mkdir -p /app/models")
+            .run_command("ln -sf /usr/bin/pip3 /usr/local/bin/pip")
+            .run_command("pip install torch==2.7.1 torchvision --index-url https://download.pytorch.org/whl/cu128")
+            .run_command("CPATH='/usr/local/cuda/include:$CPATH' pip3 install diso==0.1.4")
+            .run_command("pip install git+https://github.com/404-Repo/tripo-sg-generator-chutes.git")
 
-    # Set working directory
-    .set_workdir("/app")
-)
+            # Set up model cache directory
+            .run_command("mkdir -p /app/models")
+
+            # Set working directory
+            .set_workdir("/app")
+        )
