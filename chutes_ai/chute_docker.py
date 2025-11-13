a8ca89dadd9a4aed2efdf30c8699d666abb7d426
teshate <tereshin.tesha@gmail.com>
2025-11-13 10:30:41 +0000

    finalized docker image

--- chutes_ai/chute_docker.py
+++ chutes_ai/chute_docker.py
@@ -8,19 +8,27 @@ class ChuteDockerImage:
             .from_base("nvidia/cuda:12.8.0-devel-ubuntu24.04")
 
             # installing system libraries
-            .run_command("apt update && apt install -y python3-full python3-dev python3-pip python3-wheel git build-essential libpq-dev")
+            .run_command("apt update -y && apt-get install software-properties-common -y && apt update -y \
+                          && add-apt-repository ppa:deadsnakes/ppa -y && apt update -y")
+            .run_command("apt install -y python3.11 python3-pip python3-wheel libcudnn9-cuda-12 \
+                          gcc-14 g++-14 git build-essential libgl1 libglu1-mesa build-essential libpq-dev cmake ninja-build")
+            .run_command("apt-get remove python3-cryptography -y")
 
-            # installing python packages
+            # rebinding names pip3 -> pip; python3 -> python; gcc-14 -> gcc; g++-14 -> g++
             .run_command("rm /usr/lib/python3*/EXTERNALLY-MANAGED")
+            .run_command("update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 120 && \
+                          update-alternatives --install /usr/bin/python python /usr/bin/python3 120 && \
+                          update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 100 && \
+                          update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-14 100")
 
-            .run_command("ln -sf /usr/bin/pip3 /usr/local/bin/pip && ln -s /usr/bin/python3 /usr/bin/python")
             .run_command("pip install torch==2.7.1 torchvision --index-url https://download.pytorch.org/whl/cu128")
-            .run_command("CPATH='/usr/local/cuda/include:$CPATH' pip3 install diso==0.1.4")
-            .run_command("pip install git+https://github.com/404-Repo/tripo-sg-generator-chutes.git")
+            .run_command("CPATH='/usr/local/cuda/include:$CPATH' pip install git+https://github.com/404-Repo/tripo-sg-generator-chutes.git")
 
             # Set up model cache directory
             .run_command("mkdir -p /app/models")
 
             # Set working directory
             .set_workdir("/app")
+            .add("./chute_docker.py", "/app/chute_docker.py")
+            .add("./chute_io_data_structures.py", "/app/chute_io_data_structures.py")
         )
