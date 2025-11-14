c0094f05863213959a7dd48b44fcf3a6d2c227db
teshate <tereshin.tesha@gmail.com>
2025-11-14 08:28:34 +0000

    removed 2 redundant pip installs

--- setup_env.sh
+++ setup_env.sh
@@ -66,9 +66,7 @@ CPATH="$CONDA_PREFIX/targets/x86_64-linux/include:$CONDA_PREFIX/include"
 LD_LIBRARY_PATH="$CONDA_PREFIX/targets/x86_64-linux/lib:$CONDA_PREFIX/lib"
 
 pip install --index-url https://download.pytorch.org/whl/cu128 torch==2.7.1
-pip install numpy==2.2.6
 pip install -r requirements.txt
-pip install diffrp-nvdiffrast --no-cache-dir
 pip install -e .
 
 # Store the path of the Conda interpreter
